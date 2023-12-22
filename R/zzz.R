224f636d76fdb06407cf74c560a22adac711babe
Xiang Ge Luo <xgluo9405@gmail.com>
2023-12-22 09:46:23 +0100

    Add prompt message

---
+++ R/zzz.R
@@ -0,0 +1,13 @@
+.onLoad <- function(libname, pkgname) {
+    msg <- paste0(
+        "==================================================================\n",
+        "               Welcome to the ", pkgname, " package!\n",
+        "==================================================================\n\n",
+        "This package is developed by the Computational Biology Group\n",
+        "affiliated with the Swiss Institute of Bioinformatics (SIB).\n\n",
+        "Please cite the following paper when using this package:\n",
+        "https://www.nature.com/articles/s41467-023-39400-w\n\n",
+        "For any questions, please contact niko.beerenwinkel@bsse.ethz.ch\n\n"
+    )
+    cat(msg)
+}
